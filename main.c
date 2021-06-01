72cb43a9e7eefa7f3da485ad4f9fc22773177a99
ZXM250250 <2994250239@qq.com>
2021-06-01 12:29:09 +0800

    覆盖提交

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 34d627cce1dee1f85a31d1e3d2c8f274660e3293
+
+
 int main()
 {
     int choice;
@@ -15,22 +14,14 @@ int main()
         }
         if (choice == 1)
         {
-            //print_Map();
-            //start_Game();
-        }
-        else transfer(choice);
-    }
-<<<<<<< HEAD
-    if (choice == 1)                      //表示开始游戏
-    {
-        printMap();   //绘制游戏界面的地图
+            printMap();   //绘制游戏界面的地图
 
         showScoreTips();   //绘制游戏所得的分数和提示选项
 
         initSnake();   //初始化贪吃蛇
+        }
+        else transfer(choice);
     }
-    else //tansfer(choice);
-=======
->>>>>>> 34d627cce1dee1f85a31d1e3d2c8f274660e3293
+
     return 0;
 }
